a7273a7de94151f28e5b8f11dc02e66e496f2fca
Feka7 <feka7@hotmail.it>
2022-06-06 20:11:58 +0200

    change contract address

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -125,7 +125,7 @@ const Home: NextPage = (props) => {
       const [load, isLoading] = useState(false)
       const [{ data, error, loading }, write] = useContractWrite(
         {
-          addressOrName: '0x6be666719e7346488c795006EEd2Abe2d33b8E4c',
+          addressOrName: '0xC8b05f4ABaB41A84a1822F072C569DCEc0048C25',
           contractInterface: Abi.abi,
         },
         'mint',
